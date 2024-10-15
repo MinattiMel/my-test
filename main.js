b99eaf1f47f3c9850c6f16da92c940d2b5863b8a
Maicol Zenatti <maicol@pigmentolab.com>
2024-10-15 14:41:10 +0200

    added DOMContentLoeaded

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 import './index.css';
 
-
+document.addEventListener("DOMContentLoaded", () => {
+    // qui ci va il codice da eseguire una volta che la pagina è completamente caricata
+    console.log("DOM caricato");
+});
 
